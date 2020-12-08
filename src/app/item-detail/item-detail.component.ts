4a8e85618cf4571698bcf43eb47b0c1969d78e27
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-12-08 23:15:09 +0530

    Item Detail

---
+++ src/app/item-detail/item-detail.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+// import { Item } from '../item';
+// import { ITEMS } from '../mock-items';
+
+@Component({
+  selector: 'app-item-detail',
+  templateUrl: './item-detail.component.html',
+  styleUrls: ['./item-detail.component.css']
+})
+export class ItemDetailComponent implements OnInit {
+
+  @Input() childItem: string;
+
+  // items = ITEMS;
+
+
+  currentItem = 'bananas in boxes';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
