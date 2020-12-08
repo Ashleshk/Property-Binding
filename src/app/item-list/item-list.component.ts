ddf3cbfa6813f069d9801a8cd931d614bf1b8d1f
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-12-08 23:15:18 +0530

    item List

---
+++ src/app/item-list/item-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { ITEMS } from '../mock-items';
+import { Item } from '../item';
+
+@Component({
+  selector: 'app-item-list',
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.css']
+})
+export class ItemListComponent {
+  listItems = ITEMS;
+  @Input() items: Item[];
+  constructor() { }
+
+}
